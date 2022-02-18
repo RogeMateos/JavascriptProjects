b190238d91a3231f2e719ffa805628d43c2b81d9
Rogemateos <rogemateos@hotmail.com>
2022-02-18 17:22:28 +0000

    Adding Variables

---
+++ 01 Shop cart/js/app.js
@@ -0,0 +1,7 @@
+//Variables
+
+const carrito = document.querySelector('#carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
+let articulosCarrito = [];
