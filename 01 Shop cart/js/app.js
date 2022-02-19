5bd0634b989f40c3c1355bb890a10f689e03ea9b
Rogemateos <rogemateos@hotmail.com>
2022-02-19 00:47:22 +0000

    Linking event listener

--- 01 Shop cart/js/app.js
+++ 01 Shop cart/js/app.js
@@ -4,4 +4,16 @@ const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
-let articulosCarrito = [];
+
+
+cargarEventListeners();
+function cargarEventListeners() {
+    // Dispara cuando se presiona "Agregar Carrito"
+    listaCursos.addEventListener('click', agregarCurso);
+
+}
+
+
+function agregarCurso () { 
+    console.log('presionado en cursos')
+}
