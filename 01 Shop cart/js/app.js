8a1231cff5443377de902520bc0da5c8c4188d13
Rogemateos <rogemateos@hotmail.com>
2022-02-19 23:09:42 +0000

    Cambiando a arrow Functions

--- 01 Shop cart/js/app.js
+++ 01 Shop cart/js/app.js
@@ -5,15 +5,17 @@ const listaCursos = document.querySelector("#lista-cursos");
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
 const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
 
-cargarEventListeners();
-function cargarEventListeners() {
-  // Dispara cuando se presiona "Agregar Carrito"
-  listaCursos.addEventListener("click", agregarCurso);
-}
 
-function agregarCurso(e) {
+
+const  agregarCurso = (e) => {
   e.preventDefault(); // Previene el link comportamiento nativo
   if (e.target.classList.contains("agregar-carrito")) {
     console.log(e.target); //saber que curso estamos agregando 
   }
 }
+
+const  cargarEventListeners = () => {
+    // Dispara cuando se presiona "Agregar Carrito"
+    listaCursos.addEventListener("click", agregarCurso);
+  }
+  cargarEventListeners();
