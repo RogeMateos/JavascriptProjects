e2d60e5b4f34cbf8de02af17c6f29a64c898b995
Rogemateos <rogemateos@hotmail.com>
2022-02-19 01:11:16 +0000

    Adding e.target  when we click links

--- 01 Shop cart/js/app.js
+++ 01 Shop cart/js/app.js
@@ -14,6 +14,6 @@ function cargarEventListeners() {
 function agregarCurso(e) {
   e.preventDefault(); // Previene el link comportamiento nativo
   if (e.target.classList.contains("agregar-carrito")) {
-    console.log("agregando al carrito ...");
+    console.log(e.target); //saber que curso estamos agregando 
   }
 }
